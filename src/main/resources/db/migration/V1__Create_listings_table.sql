253fb62e20b504f1bb81201707fc46012c6c7e6a
Rafael Marinaro Verona <rverona@uolinc.com>
2019-08-17 23:10:31 -0300

    Create indexes on most frequent search criteria

--- src/main/resources/db/migration/V1__Create_listings_table.sql
+++ src/main/resources/db/migration/V1__Create_listings_table.sql
@@ -10,3 +10,8 @@ CREATE TABLE listings (
     dealer_id VARCHAR NOT NULL,
     PRIMARY KEY(code, dealer_id)
 );
+
+CREATE INDEX model_index ON listings (model);
+CREATE INDEX model_and_year_index ON listings (model, year);
+CREATE INDEX model_and_color_index ON listings (model, color);
+CREATE INDEX model_and_year_and_color_index ON listings (model, year, color);
